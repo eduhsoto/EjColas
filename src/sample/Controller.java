94d28fc6a2289087c821ede690b703d7830462ac
Luciano Martinez <cmluciano2901@gmail.com>
2017-11-19 18:46:54 -0600

    Implementacion de metodos

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -22,11 +23,12 @@ public class Controller implements Initializable {
     HBox contenedor;
 
     @FXML
-    TextField txtNumber;
+    TextField txtNumber, txtBuscar;
 
     @FXML
-    Button addBtn;
+    Button addBtn, buscarBtn, borrarBtn;
 
+    private int index = 0;
     private Nodo frente  = null;
 
   private Nodo ultimo;
@@ -40,6 +42,20 @@ public class Controller implements Initializable {
             @Override
             public void handle(ActionEvent event) {
                 insertar();
+            }
+        });
+
+        this.buscarBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                buscar();
+            }
+        });
+
+        this.borrarBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                extraer();
             }
         });
 
@@ -57,25 +73,26 @@ public class Controller implements Initializable {
            }
             temp.setProx(nuevo);
         }
-
-
+        index++;
         mostrar();
     }
 
     public void mostrar(){
-      if (frente != null){
+      //if (frente != null){
+            contenedor.getChildren().clear();
             Nodo temp = frente;
 
-            while(temp!=null){
+            //while(temp!=null) { //existe otra forma para mostrar los valores de la cola
+            for (int x= 0; x<index; x++){
                 HBox hBox = new HBox();
                 VBox vBox = new VBox();
                 ImageView hombre = null;
                 try {
-                        File archivo = new File("src/sample/Imagenes/hombre.png");
-                        Image image = new Image(archivo.toURI().toURL().toString());
-                        hombre = new ImageView(image);
+                    File archivo = new File("src/sample/Imagenes/hombre.png");
+                    Image image = new Image(archivo.toURI().toURL().toString());
+                    hombre = new ImageView(image);
 
-                }catch (Exception e){
+                } catch (Exception e) {
 
                 }
                 vBox.setAlignment(Pos.CENTER);
@@ -89,7 +106,66 @@ public class Controller implements Initializable {
                 contenedor.getChildren().add(hBox);
                 temp = temp.getProximo();
             }
-        }
+        //}
 
+    }
+
+    //Método para búscar en la cola
+    public void buscar(){
+        contenedor.getChildren().clear();
+        Nodo temp = frente;
+        if (frente == null){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Practica Colas");
+            alert.setHeaderText("La cola está vacía.");
+            alert.setContentText("Intentalo más tarde...");
+            alert.showAndWait();
+        }else{
+            for (int x= 0; x<index; x++){
+                HBox hBox = new HBox();
+                VBox vBox = new VBox();
+                ImageView mujer = null;
+                try {
+                    File archivo = new File("src/sample/Imagenes/hombre.png");
+                    Image image = new Image(archivo.toURI().toURL().toString());
+                    mujer = new ImageView(image);
+
+                } catch (Exception e) {
+
+                }
+                vBox.setAlignment(Pos.CENTER);
+
+
+                Label cola = new Label(String.valueOf(temp.getValor()));
+                cola.setStyle("-fx-font-size:18px;");
+                if (String.valueOf(temp.getValor()).equals(txtBuscar.getText())){
+                    cola.setStyle("-fx-background-color: #0ff");
+                }
+                hBox.getChildren().add(vBox);
+                vBox.getChildren().add(cola);
+                if (mujer != null){
+                    vBox.getChildren().add(mujer);
+                }
+                contenedor.getChildren().add(hBox);
+                temp = temp.getProximo();
+            }
+
+        }
+    }
+
+    //Método para eliminar elemento de la cola
+    public void extraer() {
+        if(frente == null) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Practica Colas");
+            alert.setHeaderText("La cola está vacía.");
+            alert.setContentText("Intentalo más tarde...");
+            alert.showAndWait();
+        }else {
+            int valorExtraer = frente.getValor();//Variable temporal
+            frente = frente.getProximo();//Cambiar el frente por el siguiente
+            index--;
+            mostrar();
+        }
     }
 }
